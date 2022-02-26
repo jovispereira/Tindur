5269a5a3fa4e65b9e7c044ef8d1ddb3734d681b0
jovispereira <>
2022-02-26 14:50:42 -0300

    Update ClimateAPI for work of JSON and get data

--- app/src/main/java/com/example/tindur/services/ClimateAPI.java
+++ app/src/main/java/com/example/tindur/services/ClimateAPI.java
@@ -5,6 +5,8 @@ import android.util.JsonReader;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,24 +20,38 @@ public class ClimateAPI {
     private static final String API_KEY = "/?key=af2dd6bc";
     private static final String API = "https://api.hgbrasil.com/weather";
     private static HttpsURLConnection hgWeather;
+    public String description, city, currently, temp;
+    public String JSON;
 
-    private void readResponse(InputStream response) throws IOException {
-        InputStreamReader responseBodyReader = new InputStreamReader(response);
-        JsonReader jsonReader = new JsonReader(responseBodyReader);
-        jsonReader.beginObject();
-        while(jsonReader.hasNext()) {
 
-            String value = jsonReader.toString();
-            System.out.println(jsonReader);
-             // Log.i("api", value);
-            jsonReader.skipValue();
-        }
+    private String getParameterJSON(String parameter) {
+        return JSON.split("\"".concat(parameter).concat("\":"))[1].split(",")[0].replaceAll("\"", "");
     }
 
 
+    private void readResponse(InputStream response) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String line = bufferedReader.readLine();
+        while(line != null) {
+            stringBuilder.append(line).append("\n");
+            line = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+        this.JSON = stringBuilder.toString();
+        description = getParameterJSON("description");
+        city = getParameterJSON("city");
+        temp = getParameterJSON("temp");
+        currently = getParameterJSON("currently");
+
+
+    }
+
     public ClimateAPI(String query) {
         try {
-            URL api = new URL(API.concat(API_KEY));
+            URL api = new URL(API.concat(API_KEY).concat(query));
             hgWeather = (HttpsURLConnection) api.openConnection();
             hgWeather.setRequestProperty("User-Agent", "tindur-app-v0.1");
             if(hgWeather.getResponseCode() == 200) {
