9b024279344e37e1cc8ab95cf4cc9097409e2bc2
jovispereira <>
2022-02-26 14:51:00 -0300

    Update Page for get data API and show of screen

--- app/src/main/java/com/example/tindur/MainActivity.java
+++ app/src/main/java/com/example/tindur/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tindur;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,27 +13,27 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.tindur.services.ClimateAPI;
-import com.example.tindur.utils.Utils;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity implements LocationListener {
     protected LocationManager locationManager;
     protected LocationListener locationListener;
-
-    protected String clima = "";
+    protected boolean isLoadingTexts = false;
+    protected ClimateAPI climaAPI = null;
 
 
     public void makeRequest(View view) {
-        AsyncTask.execute(() -> {
-            ClimateAPI climateAPI = new ClimateAPI("");
-            Log.i("api", "Tabom eae ?");
-        });
+
     }
 
     @SuppressLint("MissingPermission")
@@ -39,26 +41,50 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Utils utils = new Utils(getBaseContext());
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 10);
+        } else {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+    }
 
-        utils.locationPermissionRequest.launch(new String[] {
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-        });
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            finish();
+        } else {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        if(clima.equals("")) {
+        if(climaAPI == null) {
+            AsyncTask.execute(() -> {
+                String latitute = String.valueOf(location.getLatitude());
+                String longitute = String.valueOf(location.getLongitude());
+                climaAPI = new ClimateAPI("&lat=".concat(latitute).concat("&lon=").concat(longitute).concat("&user_ip=remote"));
 
-            clima = "Opa";
+            });
 
-        } else {
-            System.out.println("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-            System.out.println(clima);
+        } else if(!isLoadingTexts) {
+
+            TextView description = (TextView) findViewById(R.id.descriptionTime);
+            TextView city = (TextView) findViewById(R.id.cityLocal);
+            TextView temp = (TextView) findViewById(R.id.climateTemperature);
+            TextView currently = (TextView) findViewById(R.id.dayPeriod);
+
+            description.setText(climaAPI.description);
+            city.setText(climaAPI.city);
+            temp.setText(climaAPI.temp.concat("ºC"));
+            currently.setText(climaAPI.currently);
+            isLoadingTexts = true;
         }
 
     }
